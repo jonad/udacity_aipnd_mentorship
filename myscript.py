7064d62b278f8ebb0b1c3cdb99344e2fc2f6fa5c
jonad <jonadjons@gmail.com>
2019-04-26 14:57:33 -0700

    script and oop

---
+++ myscript.py
@@ -0,0 +1,18 @@
+
+def greetings(name):
+    '''
+    Say hello to someone
+    :param name: name of the person
+    :return:
+    '''
+    print(f'Hello {name}!')
+    
+def add(x, y):
+    '''
+    Add two integer
+    :param x: first integer
+    :param y: second integer
+    :return:
+    '''
+    return x + y
+    
